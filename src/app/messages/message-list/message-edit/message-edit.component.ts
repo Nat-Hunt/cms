566e22ddbe93f5f6aa54dc15660cabe6149a3b25
Nat-Hunt <83727447+Nat-Hunt@users.noreply.github.com>
2023-05-05 16:19:38 -0500

    Message component

--- src/app/messages/message-list/message-edit/message-edit.component.ts
+++ src/app/messages/message-list/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent {
+  @ViewChild('msgText') msgTextInput: ElementRef;
+  @ViewChild('subject') subjectInput: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
 
+  currentSender: string = "Nathan";
+
+  onSendMessage(){
+    const subject = this.subjectInput.nativeElement.value;
+    const msgText = this.msgTextInput.nativeElement.value;
+
+    const newMessage = new Message(123, subject, msgText, this.currentSender);
+
+    this.addMessageEvent.emit(newMessage);
+  }
+  onClear(){
+    this.subjectInput.nativeElement.value = "";
+    this.msgTextInput.nativeElement.value = "";
+  }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent {
+  messages: Message[] = [
+    new Message(1, 'test1', 'this is the first test', 'Nathan'),
+    new Message(2, 'test2', 'this is the second test', 'Nathan'),
+    new Message(3, 'test3', 'this is the third test', 'Nathan'),
+  ]
 
+  onAddMessage(message: Message){
+    this.messages.push(message);
+  }
 }
