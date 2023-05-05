7c632adcfd4d8731bb39ef9276d39e77135a37e8
Nat-Hunt <83727447+Nat-Hunt@users.noreply.github.com>
2023-05-05 15:11:22 -0500

    contact-detail component moved under contact-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactDetailComponent } from './contacts/contact-list/contact-detail/contact-detail.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactItemComponent } from './contacts/contact-list/contact-item/contact-item.component';
 
