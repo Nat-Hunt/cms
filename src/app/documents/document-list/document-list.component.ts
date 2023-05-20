e75df46eb9db1ec0ae26feeb3b930c56972bbcbd
Nat-Hunt <83727447+Nat-Hunt@users.noreply.github.com>
2023-05-20 12:18:11 -0500

    document service implemented

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,23 +1,23 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent {
-  documents: Document[] = [
-    new Document(1, 'Test Doc 1', 'This is the first test document', 'fakeUrl', []),
-    new Document(2, 'Test Doc 2', 'This is the second test document', 'fakeUrl', []),
-    new Document(3, 'Test Doc 3', 'This is the third test document', 'fakeUrl', []),
-    new Document(4, 'Test Doc 4', 'This is the fourth test document', 'fakeUrl', []),
-    new Document(5, 'Test Doc 5', 'This is the fifth test document', 'fakeUrl', []),
-  ]
-  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+export class DocumentListComponent implements OnInit{
+  documents: Document[] = [];
+
+  constructor(private documentService: DocumentService){}
 
   onSelectedDocument(document: Document){
-    this.selectedDocumentEvent.emit(document);
+    this.documentService.documentSelectedEvent.emit(document);
+  }
+
+  ngOnInit(): void {
+      this.documents = this.documentService.getDocuments();
   }
 
 }
