973ce7223519b6b221c33f0306fee5bc8563a53a
Nat-Hunt <83727447+Nat-Hunt@users.noreply.github.com>
2023-06-17 17:58:21 -0500

    HTTP Get/Put requests on MessageService

--- src/app/messages/message.service.ts
+++ src/app/messages/message.service.ts
@@ -1,20 +1,72 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  private messageDatabaseUrl: string = 'https://wdd430-cms-4efbe-default-rtdb.firebaseio.com/messages.json';
   private messages: Message[];
   messageChangedEvent = new EventEmitter<Message[]>();
+  messageListChangedEvent = new Subject<Message[]>();
+  maxMessageId: number;
 
-  constructor() {
+  constructor(
+    private http: HttpClient
+  ) {
     this.messages = MOCKMESSAGES;
   }
 
-  getMessages(): Message[] {
-    return this.messages.slice()
+  getMaxId(): number {
+    let maxId = 0;
+
+    for (let message of this.messages) {
+      let currentId = +message.id;
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+
+    return maxId
+  }
+
+  storeMessages() {
+    const messages = JSON.stringify(this.messages);
+    let httpHeaders = new HttpHeaders;
+    httpHeaders.set('Content-type', 'application/json');
+    this.http
+      .put(this.messageDatabaseUrl, messages, {headers: httpHeaders})
+      .subscribe(()=> {
+        let messageListClone = this.messages.slice();
+        this.messageListChangedEvent.next(messageListClone);
+      })
+  }
+
+  getMessages() {
+    this.http
+      .get<Message[]>(this.messageDatabaseUrl)
+      .subscribe(
+        (messages: Message[]) => {
+          this.messages = messages;
+          this.maxMessageId = this.getMaxId();
+
+          this.messages.sort((a, b)=> {
+            if (a.id > b.id) {
+              return 1;
+            } else {
+              return -1;
+            }
+          })
+          let messageListClone = this.messages.slice();
+          this.messageListChangedEvent.next(messageListClone);
+        },
+        (error: any) => {
+          console.log(error)
+        }
+      )
   }
 
   getMessage(id: string): Message {
@@ -28,7 +80,7 @@ export class MessageService {
 
   addMessage(message: Message) {
     this.messages.push(message);
-    this.messageChangedEvent.emit(this.messages.slice())
+    this.storeMessages();
   }
 
 }
